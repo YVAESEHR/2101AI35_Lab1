b6fbc71baa144a624d72132f546bae3b08964309
YASHVEER <yashahlawat221b@gmail.com>
2022-07-27 21:36:54 +0530

    QUICK Sort

--- sorting.c
+++ sorting.c
@@ -99,6 +99,44 @@ void merge(int arr[], int l, int m, int r)
     }
 }
 
+// Quick Sort
+void quickSort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+        int q = partition(arr, l, r);
+        quickSort(arr, l, q - 1);
+        quickSort(arr, q + 1, r);
+    }
+}
+
+int partition(int arr[], int l, int r)
+{
+    int i = l - 1;
+    int j = r;
+    int pivot = arr[r];
+    while (1)
+    {
+        while (arr[++i] < pivot)
+            ;
+        while (arr[--j] > pivot)
+            ;
+        if (i < j)
+            swap(&arr[i], &arr[j]);
+        else
+            break;
+    }
+    swap(&arr[i], &arr[r]);
+    return i;
+}
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 int main()
 {
     printf("Choose one of the following:\n");
@@ -188,6 +226,27 @@ int main()
             scanf("%d", &arr[i]);
         }
         mergeSort(arr, 0, n - 1);
+        printf("The sorted array is: ");
+        for (int i = 0; i < n; i++)
+        {
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+        break;
+    }
+    case 5:
+    {
+        printf("Quick Sort :-\n");
+        int n;
+        printf("Enter the number of elements in the array: ");
+        scanf("%d", &n);
+        int arr[n];
+        printf("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d", &arr[i]);
+        }
+        quickSort(arr, 0, n - 1);
         printf("The sorted array is: ");
         for (int i = 0; i < n; i++)
         {
