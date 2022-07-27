85ba70e5097fa31794e669fce9989c686a91c3d5
YASHVEER <yashahlawat221b@gmail.com>
2022-07-27 20:11:28 +0530

    SUBTRACTION Function

--- calculator.c
+++ calculator.c
@@ -5,6 +5,11 @@ float add(float a, float b)
         return a + b;
     }
 
+float subtract(float a, float b)
+    {
+        return a - b;
+    }
+
 int main()
 {
     printf("Choose an operation (+,-,*,/) : ");
@@ -17,6 +22,12 @@ int main()
         float num1, num2;
         scanf("%f %f", &num1, &num2);
         printf("The sum of %f and %f is : %f\n", num1, num2, add(num1, num2));
+        break;
+    case '-':
+        printf("Enter two numbers to subtract :\n");
+        float num3, num4;
+        scanf("%f %f", &num3, &num4);
+        printf("The difference of %f and %f is : %f\n", num3, num4, subtract(num3, num4));
         break;
     return 0;
 }
